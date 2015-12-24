f75a1b5e801bb890dbe92ab65849871628118151
Fanfan-Li <Fanfan-Li@outlook.com>
2015-12-24 16:46:51 +0800

    16进制颜色

---
+++ UIColor+Her.h
@@ -0,0 +1,21 @@
+//
+//  UIColor+Her.h
+//  JieMian
+//
+//  Created by 千锋 on 15/12/18.
+//  Copyright (c) 2015年 千锋. All rights reserved.
+//
+// 16进制颜色
+#import <UIKit/UIKit.h>
+
+#define RGBA_COLOR(R, G, B, A) [UIColor colorWithRed:((R) / 255.0f) green:((G) / 255.0f) blue:((B) / 255.0f) alpha:A]
+#define RGB_COLOR(R, G, B) [UIColor colorWithRed:((R) / 255.0f) green:((G) / 255.0f) blue:((B) / 255.0f) alpha:1.0f]
+
+@interface UIColor (Hex)
+
++ (UIColor *)colorWithHexString:(NSString *)color;
+
+//从十六进制字符串获取颜色，
+//color:支持@“#123456”、 @“0X123456”、 @“123456”三种格式
++ (UIColor *)colorWithHexString:(NSString *)color alpha:(CGFloat)alpha;
+@end
